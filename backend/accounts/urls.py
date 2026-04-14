96a00ea1056e9acf56deaa11d32393566366aef7
Mahmudul Hasan Hasib <125702889+mahmudul-hasan-hasib@users.noreply.github.com>
2026-04-14 15:15:48 +0600

    add admin route

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from .views import RegisterAPIView, MeAPIView
+from .views import RegisterAPIView, MeAPIView, create_admin
 
 urlpatterns = [
     path("register/", RegisterAPIView.as_view(), name="register"),
     path("login/", TokenObtainPairView.as_view(), name="login"),
     path("refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("me/", MeAPIView.as_view(), name="me"),
+    path("create-admin/", create_admin),
 ]
--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,7 +1,8 @@
 from rest_framework import generics, permissions
 from rest_framework.response import Response
 from .serializers import RegisterSerializer, UserSerializer
-
+from django.contrib.auth import get_user_model
+from django.http import HttpResponse
 
 class RegisterAPIView(generics.CreateAPIView):
     serializer_class = RegisterSerializer
@@ -14,3 +15,21 @@ class MeAPIView(generics.GenericAPIView):
     def get(self, request):
         serializer = UserSerializer(request.user)
         return Response(serializer.data)
+    
+
+from django.contrib.auth import get_user_model
+from django.http import HttpResponse
+
+
+def create_admin(request):
+    User = get_user_model()
+
+    if not User.objects.filter(username="admin").exists():
+        User.objects.create_superuser(
+            username="admin",
+            email="admin@example.com",
+            password="admin123"
+        )
+        return HttpResponse("Admin created")
+
+    return HttpResponse("Admin already exists")
